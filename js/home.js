c6a15687e6b7133f3a0d36cf69cbdf696ffe437f
Brooke <b.lianahargrove@gmail.com>
2017-10-31 22:33:11 -0400

    Improve Responsivess

--- js/home.js
+++ js/home.js
@@ -1,9 +1,16 @@
-$(window).on("scroll", function() {
-    if($(window).scrollTop() > 600) {
-        $("header").addClass("active");
-        $(".logo").fadeIn();
-    } else {
-      $(".logo").fadeOut();
-       $("header").removeClass("active");
-    }
+$(window).on('load', function() {
+  $(window).on("scroll", function() {
+      if($(window).scrollTop() > 600) {
+          $("header").addClass("active");
+          $("nav.desktop .logo").fadeIn();
+      } else {
+        $("nav.desktop .logo").fadeOut();
+         $("header").removeClass("active");
+      }
+  });
+
+  $('.hamburger-menu').on('click', function(e){
+    e.preventDefault();
+    $('nav.mobile ul').slideToggle(200);
+  })
 });
